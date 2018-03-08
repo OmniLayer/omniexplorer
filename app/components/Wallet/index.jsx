5fbf3d03c81e60308c2b4d64466aa370f1eb7433
germanf <ferreyra.ger@gmail.com>
2018-03-08 19:25:31 -0300

    + #refactoring moved Wallet & AddressDetail to stateless components to avoid double fetch on parent render

--- app/components/Wallet/index.jsx
+++ app/components/Wallet/index.jsx
@@ -23,8 +23,7 @@ const StyledTH = styled.th`
       border: none !important;
     `;
 
-// class Wallet extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-const Wallet = (props) => (
+const Wallet = (props) => ( // eslint-disable-line react/prefer-stateless-function
   <Container fluid>
     <DetailRow>
       <Col className="col-auto mr-auto" sm="2">
@@ -33,38 +32,38 @@ const Wallet = (props) => (
       <Col>
         <Table className="table-profile">
           <thead>
-          <tr>
-            <StyledTH></StyledTH>
-            <StyledTH>
-              <h4>
-                <span className="d-block" id="laddress">{props.addr}</span>
-              </h4>
-            </StyledTH>
-          </tr>
+            <tr>
+              <StyledTH></StyledTH>
+              <StyledTH>
+                <h4>
+                  <span className="d-block" id="laddress">{props.addr}</span>
+                </h4>
+              </StyledTH>
+            </tr>
           </thead>
           <tbody>
-          <tr className="highlight">
-            <StyledTD className="field">Balances</StyledTD>
-            <StyledTD>
-              <table className="table" style={{ marginBottom: '5px' }}>
-                <thead>
-                <tr>
-                  <StyledTH></StyledTH>
-                  <StyledTH>ID</StyledTH>
-                  <StyledTH>Name</StyledTH>
-                  <StyledTH className="text-right">Reserved Balance</StyledTH>
-                  <StyledTH className="text-right">Available Balance</StyledTH>
-                </tr>
-                </thead>
-                <tbody>
-                { (props.address.balance || []).map((balance) => <Token {...balance} key={balance.id}/>) }
-                </tbody>
-              </table>
-            </StyledTD>
-          </tr>
-          <tr className="divider">
-            <td colSpan="2"></td>
-          </tr>
+            <tr className="highlight">
+              <StyledTD className="field">Balances</StyledTD>
+              <StyledTD>
+                <table className="table" style={{ marginBottom: '5px' }}>
+                  <thead>
+                    <tr>
+                      <StyledTH></StyledTH>
+                      <StyledTH>ID</StyledTH>
+                      <StyledTH>Name</StyledTH>
+                      <StyledTH className="text-right">Reserved Balance</StyledTH>
+                      <StyledTH className="text-right">Available Balance</StyledTH>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    { (props.address.balance || []).map((balance) => <Token {...balance} key={balance.id} />) }
+                  </tbody>
+                </table>
+              </StyledTD>
+            </tr>
+            <tr className="divider">
+              <td colSpan="2"></td>
+            </tr>
           </tbody>
         </Table>
       </Col>
