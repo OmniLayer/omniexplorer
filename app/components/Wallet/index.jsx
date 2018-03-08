2785cf117d159aecdb3e384fe72f8824d56ea3f8
germanf <ferreyra.ger@gmail.com>
2018-03-08 19:24:41 -0300

    + #refactoring moved Wallet component to stateless Component to avoid fetch twice

--- app/components/Wallet/index.jsx
+++ app/components/Wallet/index.jsx
@@ -23,57 +23,54 @@ const StyledTH = styled.th`
       border: none !important;
     `;
 
-class Wallet extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <Container fluid>
-        <DetailRow>
-          <Col className="col-auto mr-auto" sm="2">
-            <QRCode value={this.props.addr} />
-          </Col>
-          <Col>
-            <Table className="table-profile">
-              <thead>
+// class Wallet extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+const Wallet = (props) => (
+  <Container fluid>
+    <DetailRow>
+      <Col className="col-auto mr-auto" sm="2">
+        <QRCode value={props.addr} />
+      </Col>
+      <Col>
+        <Table className="table-profile">
+          <thead>
+          <tr>
+            <StyledTH></StyledTH>
+            <StyledTH>
+              <h4>
+                <span className="d-block" id="laddress">{props.addr}</span>
+              </h4>
+            </StyledTH>
+          </tr>
+          </thead>
+          <tbody>
+          <tr className="highlight">
+            <StyledTD className="field">Balances</StyledTD>
+            <StyledTD>
+              <table className="table" style={{ marginBottom: '5px' }}>
+                <thead>
                 <tr>
                   <StyledTH></StyledTH>
-                  <StyledTH>
-                    <h4>
-                      <span className="d-block" id="laddress">{this.props.addr}</span>
-                    </h4>
-                  </StyledTH>
+                  <StyledTH>ID</StyledTH>
+                  <StyledTH>Name</StyledTH>
+                  <StyledTH className="text-right">Reserved Balance</StyledTH>
+                  <StyledTH className="text-right">Available Balance</StyledTH>
                 </tr>
-              </thead>
-              <tbody>
-                <tr className="highlight">
-                  <StyledTD className="field">Balances</StyledTD>
-                  <StyledTD>
-                    <table className="table" style={{ marginBottom: '5px' }}>
-                      <thead>
-                        <tr>
-                          <StyledTH></StyledTH>
-                          <StyledTH>ID</StyledTH>
-                          <StyledTH>Name</StyledTH>
-                          <StyledTH className="text-right">Reserved Balance</StyledTH>
-                          <StyledTH className="text-right">Available Balance</StyledTH>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        { (this.props.address.balance||[]).map((balance) => <Token {...balance} key={balance.id}/>) }
-                      </tbody>
-                    </table>
-                  </StyledTD>
-                </tr>
-                <tr className="divider">
-                  <td colSpan="2"></td>
-                </tr>
-              </tbody>
-            </Table>
-          </Col>
-        </DetailRow>
-      </Container>
-    );
-  }
-}
+                </thead>
+                <tbody>
+                { (props.address.balance || []).map((balance) => <Token {...balance} key={balance.id}/>) }
+                </tbody>
+              </table>
+            </StyledTD>
+          </tr>
+          <tr className="divider">
+            <td colSpan="2"></td>
+          </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </DetailRow>
+  </Container>
+);
 
 Wallet.propTypes = {
   address: PropTypes.object.isRequired,
