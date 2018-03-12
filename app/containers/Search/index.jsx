4e23d1f2707e556e9e87473f5e39399f7747a923
germanf <ferreyra.ger@gmail.com>
2018-03-12 18:31:03 -0300

    + Search actions, constants, reducer & saga

--- app/containers/Search/index.jsx
+++ app/containers/Search/index.jsx
@@ -14,11 +14,28 @@ import { compose } from 'redux';
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
 import makeSelectSearch from './selectors';
-import reducer from './reducer';
-import saga from './saga';
+import searchReducer from './reducer';
+import searchSaga from './saga';
+import { loadSearch } from './actions';
 import messages from './messages';
 
 export class Search extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.query = props.match.params.query.toString();
+  }
+  
+  componentDidMount() {
+    debugger;
+    this.props.loadSearch(this.query);
+  }
+  
+  componentWillReceiveProps(newProps) {
+    if (newProps.match.params.query !== this.query) {
+      this.props.loadSearch(this.query);
+    }
+  }
+  
   render() {
     return (
       <div>
@@ -30,6 +47,7 @@ export class Search extends React.PureComponent { // eslint-disable-line react/p
 
 Search.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  loadSearch: PropTypes.func,
 };
 
 const mapStateToProps = createStructuredSelector({
@@ -39,13 +57,14 @@ const mapStateToProps = createStructuredSelector({
 function mapDispatchToProps(dispatch) {
   return {
     dispatch,
+    loadSearch: (query) => dispatch(loadSearch(query)),
   };
 }
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps);
 
-const withReducer = injectReducer({ key: 'search', reducer });
-const withSaga = injectSaga({ key: 'search', saga });
+const withReducer = injectReducer({ key: 'search', reducer: searchReducer });
+const withSaga = injectSaga({ key: 'search', saga: searchSaga });
 
 export default compose(
   withReducer,
--- app/containers/Search/reducer.js
+++ app/containers/Search/reducer.js
@@ -6,15 +6,30 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
+  LOAD_SEARCH,
+  LOAD_SEARCH_SUCCESS,
+  LOAD_SEARCH_ERROR,
 } from './constants';
 
 const initialState = fromJS({});
 
 function searchReducer(state = initialState, action) {
-  switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+  const { error, payload, type } = action;
+
+  switch (type) {
+    case LOAD_SEARCH:
+      return state
+        .set('loading', true)
+        .set('error', false);
+    case LOAD_SEARCH_SUCCESS:
+      return state
+        .set('error', false)
+        .set('loading', false)
+        .set('payload', payload);
+    case LOAD_SEARCH_ERROR:
+      return state
+        .set('error', error)
+        .set('loading', false);
     default:
       return state;
   }
