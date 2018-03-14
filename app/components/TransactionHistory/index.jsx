fe870c3f91efc6ddec593c2388d6b5ace1232f50
germanf <ferreyra.ger@gmail.com>
2018-03-14 14:48:10 -0300

    + coming soon stats styling

--- app/components/TransactionHistory/index.jsx
+++ app/components/TransactionHistory/index.jsx
@@ -5,17 +5,21 @@
  */
 
 import React from 'react';
-import { HorizontalGridLines, LineSeries, XAxis, XYPlot, YAxis } from 'react-vis';
+// import { HorizontalGridLines, LineSeries, XAxis, XYPlot, YAxis } from 'react-vis';
 import statsPNG from 'images/stats.png';
+import styled from 'styled-components';
 
-// import styled from 'styled-components';
+const IMG = styled.img``;
 
+const ContainerLogo = styled.div`
+  background-color: #3498db;
+`;
 
 class TransactionHistory extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <ContainerLogo className="d-flex px-3 py-2">
-        <div className="d-inline-block">
+        <div className="d-inline-block mx-auto">
           <IMG src={statsPNG} />
         </div>
       </ContainerLogo>
