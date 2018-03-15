d1695287acfb02a6ebb5992d2ae158ee1e574193
germanf <ferreyra.ger@gmail.com>
2018-03-15 16:45:16 -0300

    + removed offline first support

--- app/app.js
+++ app/app.js
@@ -121,13 +121,3 @@ if (!window.Intl) {
 } else {
   render(translationMessages);
 }
-
-// Install ServiceWorker and AppCache in the end since
-// it's not most important operation and if main code fails,
-// we do not want it installed
-if (isProd) {
-  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
-}
-
-// https://github.com/react-boilerplate/react-boilerplate/pull/98/files
-// https://github.com/react-boilerplate/react-boilerplate/pull/88/files
