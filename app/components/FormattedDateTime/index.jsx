6d5a2001d3e785039a1d068a9b856a8d4922b803
germanf <ferreyra.ger@gmail.com>
2018-03-16 18:08:32 -0300

    + FormattedDateTime & FormattedUnixDateTime components

---
+++ app/components/FormattedDateTime/index.jsx
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault(ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var _FormattedDateTime = _interopDefault(require('./FormattedDateTime'));
+var _FormattedUnixDateTime = _interopDefault(require('./FormattedUnixDateTime'));
+
+
+exports.FormattedDateTime = _FormattedDateTime;
+exports.FormattedUnixDateTime = _FormattedUnixDateTime;
