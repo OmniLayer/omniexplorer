e2289aeafdcb144c139137336c4dac09f864a2c1
germanf <ferreyra.ger@gmail.com>
2018-03-20 15:58:43 -0300

    + fix show all decimal places, trim zeros to the right

--- app/components/SanitizedFormattedNumber/index.jsx
+++ app/components/SanitizedFormattedNumber/index.jsx
@@ -5,14 +5,16 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import { FormattedNumber } from 'react-intl';
 import { DEFAULT_NOT_NUMBER } from 'containers/App/constants';
 
 function SanitizedFormattedNumber(props) {
   const isNumeric = (data) => (!isNaN(parseFloat(data)) && isFinite(data) && data.constructor !== Array);
+  const value = parseFloat(props.value, 10);
+  const valueStr = props.value.toString();
+  const minimumFractionDigits = (value !== 0 ? valueStr.slice(valueStr.indexOf('.') + 1).length : 0);
   const number = (isNumeric(props.value) ?
-      <FormattedNumber value={parseFloat(props.value, 10)} /> :
+      <FormattedNumber value={value} minimumFractionDigits={minimumFractionDigits}/> :
       <span>{ DEFAULT_NOT_NUMBER }</span>
   );
   
