104b600ff174c3320c714f33a092317768558566
germanf <ferreyra.ger@gmail.com>
2018-03-27 18:13:52 -0300

    + added enzyme helper to do mount & shallow with context (store, and intl support)

---
+++ app/tests/intl-enzyme-test-helper.js
@@ -0,0 +1,54 @@
+/**
+ * Components using the react-intl module require access to the intl context.
+ * This is not available when mounting single components in Enzyme.
+ * These helper functions aim to address that and wrap a valid,
+ * English-locale intl context around them.
+ */
+
+import React from 'react';
+import { IntlProvider, intlShape } from 'react-intl';
+import { mount, shallow } from 'enzyme';
+import configureMockStore from 'redux-mock-store';
+
+/**
+ * mock store
+ */
+const middlewares = [];
+const mockStore = configureMockStore(middlewares);
+const store = mockStore();
+/**
+ * Default messages, it'll be used when messages is not given
+ * You can pass your messages to the IntlProvider. Optional: remove if unneeded.
+ */
+const defaultMessages = require('../translations/en'); // en.json
+
+/**
+ * When using React-Intl `injectIntl` on components, props.intl is required.
+ */
+function nodeWithIntlProp(node, intl) {
+  return React.cloneElement(node, { intl, store });
+}
+
+function getIntlShape(messages = defaultMessages) {
+// Create the IntlProvider to retrieve context for wrapping around.
+  const intlProvider = new IntlProvider({ locale: 'en', messages }, {});
+  const { intl } = intlProvider.getChildContext();
+  return intl;
+}
+
+/**
+ * Export these methods.
+ */
+export function mountWithIntl(node, msgs) {
+  const intl = getIntlShape(msgs);
+
+  return mount(nodeWithIntlProp(node, intl), {
+    context: { intl, store },
+    childContextTypes: { intl: intlShape },
+  });
+}
+
+export function shallowWithIntl(node, msgs) {
+  const intl = getIntlShape(msgs);
+  return shallow(nodeWithIntlProp(node), { context: { intl, store } });
+}
