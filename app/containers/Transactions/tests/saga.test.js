b0078df275e9e6eeb66a61fe32bdd2489bb74dd9
germanf <ferreyra.ger@gmail.com>
2018-04-04 13:23:51 -0300

    + testing transactions saga

--- app/containers/Transactions/tests/saga.test.js
+++ app/containers/Transactions/tests/saga.test.js
@@ -2,31 +2,33 @@
  * Tests for Transactions sagas
  */
 
-import { put, takeLatest } from 'redux-saga/effects';
-
-import { LOAD_TRANSACTIONS } from '../constants';
-import { transactionsLoaded, transactionsLoadingError } from '../actions';
+import { all, put, takeLatest } from 'redux-saga/effects';
+import { testSaga } from 'redux-saga-test-plan';
+import { LOAD_TRANSACTIONS, LOAD_TRANSACTIONS_SUCCESS } from '../constants';
+import { transactionsLoadingError } from '../actions';
+import { API_URL_BASE } from 'containers/App/constants';
 
 import root, { getTransactions } from '../saga';
+import request from 'utils/request';
 
 const txid = 'dbf8b73aa9149ae3e8a96e85c64c48d8061d65c026b16c899e77bb6a607bd45x';
 
 /* eslint-disable redux-saga/yield-effects */
 describe('getTransaction Saga', () => {
   let getTransactionGenerator;
-
+  
   // We have to test twice, once for a successful load and once for an unsuccessful one
   // so we do all the stuff that happens beforehand automatically in the beforeEach
   beforeEach(() => {
     getTransactionGenerator = getTransactions();
-
+    
     const selectDescriptor = getTransactionGenerator.next().value;
     expect(selectDescriptor).toMatchSnapshot();
-
+    
     const callDescriptor = getTransactionGenerator.next(txid).value;
     expect(callDescriptor).toMatchSnapshot();
   });
-
+  
   it('should dispatch the transactionsLoaded action if it requests the data successfully', () => {
     const response = {
       pages: 143938,
@@ -48,11 +50,28 @@ describe('getTransaction Saga', () => {
           version: 0,
         }],
     };
-
-    const putDescriptor = getTransactionGenerator.next(response).value;
-    expect(putDescriptor).toEqual(put(transactionsLoaded(response, txid)));
+    
+    const saga = testSaga(getTransactions, { tx: txid });
+    const url = `${API_URL_BASE}/transaction/general/0`;
+    const getTransactionsOptions = {
+      type: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    
+    saga
+      .next()
+      .call(request, url, getTransactionsOptions)
+      .next(response)
+      .put({
+        type: LOAD_TRANSACTIONS_SUCCESS,
+        transactions: response.transactions,
+        pages: response.pages,
+        currentPage: 0
+      });
   });
-
+  
   it('should call the transactionsLoadingError action if the response errors', () => {
     const response = new Error('Some error');
     const putDescriptor = getTransactionGenerator.throw(response).value;
@@ -60,11 +79,16 @@ describe('getTransaction Saga', () => {
   });
 });
 
-describe('githubDataSaga Saga', () => {
-  const rootSaga = root();
-
+describe('Root Saga', () => {
   it('should start task to watch for LOAD_TRANSACTIONS action', () => {
-    const takeLatestDescriptor = rootSaga.next().value;
-    expect(takeLatestDescriptor).toEqual(takeLatest(LOAD_TRANSACTIONS, getTransactions));
+    // arrange
+    const rootSaga = root();
+    const expectedYield = all([takeLatest(LOAD_TRANSACTIONS, getTransactions)]);
+    
+    // act
+    const actualYield = rootSaga.next().value;
+    
+    // assert
+    expect(actualYield).toEqual(expectedYield);
   });
 });
