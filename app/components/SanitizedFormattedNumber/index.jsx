a7c8d79f0879402e11731b9911f0a01a5c4e48fb
germanf <ferreyra.ger@gmail.com>
2018-04-06 13:45:39 -0300

    + remove insignificant trailing zeros from sanitized numbers + unit test

--- app/components/SanitizedFormattedNumber/index.jsx
+++ app/components/SanitizedFormattedNumber/index.jsx
@@ -11,10 +11,9 @@ import { DEFAULT_NOT_NUMBER } from 'containers/App/constants';
 function SanitizedFormattedNumber(props) {
   const isNumeric = (data) => (!isNaN(parseFloat(data)) && isFinite(data) && data.constructor !== Array);
   const value = parseFloat(props.value, 10);
-  const valueStr = props.value.toString();
-  const minimumFractionDigits = (value !== 0 ? valueStr.slice(valueStr.indexOf('.') + 1).length : 0);
+  
   const number = (isNumeric(props.value) ?
-    <FormattedNumber value={value} minimumFractionDigits={minimumFractionDigits} /> :
+    <FormattedNumber value={value.toString()}/> :
     <span>{ DEFAULT_NOT_NUMBER }</span>
   );
 
--- app/components/SanitizedFormattedNumber/tests/index.test.js
+++ app/components/SanitizedFormattedNumber/tests/index.test.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { IntlProvider } from 'react-intl';
 // import ReactTestUtils from 'react-dom/test-utils';
 import { connect } from 'react-redux';
-import { shallow, render, mount } from 'enzyme';
-import { mountWithState, shallowWithState, mountWithStore } from 'enzyme-redux';
+import { mount } from 'enzyme';
+import { shallowWithState } from 'enzyme-redux';
+import SanitizedFormattedNumber from '../index';
 // import { createMockStore } from 'redux-test-utils';
 
-import SanitizedFormattedNumber from '../index';
-
 describe('<SanitizedFormattedNumber />', () => {
-  const ReactComponent = () => (<SanitizedFormattedNumber />);
+  const ReactComponent = () => (<SanitizedFormattedNumber/>);
   it('should render <SanitizedFormattedNumber />', () => {
     const expectedState = { mockedStated: true };
     const mapStateToProps = (state) => ({
@@ -20,10 +19,14 @@ describe('<SanitizedFormattedNumber />', () => {
     expect(component.props().state).toBe(expectedState);
   });
   
-  it('should format number', ()=>{
-    const component = <IntlProvider locale="en"><SanitizedFormattedNumber value={'1.001'}/></IntlProvider>;
+  it('should format number', () => {
+    const component = (
+      <IntlProvider locale="en">
+        <SanitizedFormattedNumber value={'1001.001000'}/>
+      </IntlProvider>
+    );
     const monted = mount(component);
     expect(monted.children().length).toBe(1);
-    expect(monted.text()).toBe("1.001");
+    expect(monted.text()).toBe('1,001.001');
   });
 });
