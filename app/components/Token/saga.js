d190e016b1768dbcd7f0c53eb84b135b27356a54
germanf <ferreyra.ger@gmail.com>
2018-04-23 13:28:08 -0300

    + load desired token

--- app/components/Token/saga.js
+++ app/components/Token/saga.js
@@ -1,31 +1,50 @@
-import { all, call, put, select, takeEvery } from 'redux-saga/effects';
+import { all, call, fork, put, select, takeEvery } from 'redux-saga/effects';
 import request from 'utils/request';
 
 import { API_URL_BASE } from 'containers/App/constants';
 import { LOAD_PROPERTY } from './constants';
 import { errorFetch, updateFetch } from './actions';
 
-function* fetchProperty(action) {
+function* fetchPropertyDeep(action) {
   try {
-    // @refactoring: remove the state set
     const state = yield select((st) => st);
     const tokens = state.get('token').get('tokens');
-
+    
+    // load token if is still not requested
     if (!tokens.get(action.id.toString())) {
-      const requestURL = `${API_URL_BASE}/property/${action.id}`;
-      const property = yield call(request, requestURL);
-      yield put(updateFetch(property));
+      const property = yield call(fetchProperty, action.id);
+      
+      if(!property){
+        const error = new Error(`Failed to fetch property ${action.id}`);
+        throw  error;
+      }
+      
+      // load desired property if it's still not requested
+      const propertyiddesired = (property.propertyiddesired || '').toString();
+      if (propertyiddesired && !tokens.get(propertyiddesired)) {
+        yield fork(fetchProperty, propertyiddesired);
+      }
     }
   } catch (err) {
+    console.log('SAGA fetchPropertyDeep ERR: ', err);
     yield put(errorFetch(err));
   }
 }
+
+function* fetchProperty(propertyId) {
+  const requestURL = `${API_URL_BASE}/property/${propertyId}`;
+  const property = yield call(request, requestURL);
+  yield put(updateFetch(property));
+  
+  return property;
+}
+
 
 /**
  * Root saga manages watcher lifecycle
  */
 export default function* root() {
   yield all([
-    takeEvery(LOAD_PROPERTY, fetchProperty),
+    takeEvery(LOAD_PROPERTY, fetchPropertyDeep),
   ]);
 }
