2f3cbb678d9a9e2bd0f72860a7301cbe169e878d
germanf <ferreyra.ger@gmail.com>
2018-04-26 20:58:54 -0300

    + datetime validation for unix timestamps

--- app/components/FormattedDateTime/FormattedUnixDateTime.js
+++ app/components/FormattedDateTime/FormattedUnixDateTime.js
@@ -8,7 +8,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FormattedDateTime from './FormattedDateTime';
 
-const FormattedUnixDateTime = ({ datetime }) => <FormattedDateTime datetime={datetime * 1000} />;
+const FormattedUnixDateTime = ({ datetime }) => (
+  ((new Date(datetime * 1000)).getTime() > 0) ?
+    (<FormattedDateTime datetime={datetime * 1000} />) :
+    (<span> invalid date</span>)
+);
 
 FormattedUnixDateTime.propTypes = {
   datetime: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.instanceOf(Date)]),
