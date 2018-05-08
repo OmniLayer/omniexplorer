8f78f645e5b3364c0c9c4bdd0a0130f49dc59a6b
germanf <ferreyra.ger@gmail.com>
2018-05-08 20:07:25 -0300

    + crowdsale detail #wip

--- app/containers/CrowdsaleDetail/index.jsx
+++ app/containers/CrowdsaleDetail/index.jsx
@@ -11,7 +11,7 @@ import { connect } from 'react-redux';
 import { routeActions } from 'redux-simple-router';
 import { Link } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
-import { Button, Card, CardBody, CardText, CardTitle, Col, Row, Table } from 'reactstrap';
+import { Button, Card, CardBody, CardText, CardTitle, Col, ListGroup, ListGroupItem, Row, Table, Container } from 'reactstrap';
 import styled from 'styled-components';
 
 import injectSaga from 'utils/injectSaga';
@@ -19,6 +19,9 @@ import injectReducer from 'utils/injectReducer';
 import { startDeepFetch } from 'components/Token/actions';
 import { makeSelectProperty } from 'components/Token/selectors';
 import SanitizedFormattedNumber from 'components/SanitizedFormattedNumber';
+import LoadingIndicator from 'components/LoadingIndicator';
+import Moment from 'react-moment';
+
 // Icons
 import FacebookIcon from 'react-icons/lib/io/social-facebook';
 import GPlusIcon from 'react-icons/lib/io/social-googleplus';
@@ -30,6 +33,7 @@ import { startCrowdsaleTransactionsFetch } from './actions';
 import reducer from './reducer';
 import saga from './saga';
 import './crowdsaledetail.scss';
+import FormattedUnixDateTime from '../../components/FormattedDateTime/FormattedUnixDateTime';
 
 const AssetDetail = styled.p`
 	font-size: 1.25rem;
@@ -42,160 +46,217 @@ const StyledCard = styled(Card).attrs({
   width: 100%;
 `;
 
+const StyledDivContent = styled.div.attrs({
+  className: 'mt-3 mb-5 mx-auto text-md-left',
+})`
+`;
+
+const StyledSpan = styled.span.attrs({
+  className: 'text-sm-center',
+})`
+  width: 90%;
+`;
+
 export class CrowdsaleDetail extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
-    
+
     this.crowdsaleid = this.props.match.params.crowdsaleid.toString();
     this.props.getPropertyDeep(this.crowdsaleid);
     this.props.getCrowdsaleTransactions(this.crowdsaleid);
   }
-  
+
   render() {
+    const loading = (
+      <Container>
+        <LoadingIndicator />
+      </Container>
+    );
+
     const crowdsale = this.props.properties(this.crowdsaleid);
-    if (!crowdsale) return null;
-    
+    if (!crowdsale) return loading;
+
     const dessiredToken = this.props.properties(crowdsale.propertyiddesired.toString());
-    if (!dessiredToken) return null;
-    
+    if (!dessiredToken) return loading;
+
     const detail = this.props.crowdsaledetail;
-    
-    
+    const TransactionLabel = (props) => (
+      props.tx.type_int === 51 ?
+        <span>{crowdsale.propertyname} was created </span> :
+        <span><SanitizedFormattedNumber value={props.tx.amount} minimunFractionDigits={2} /> {dessiredToken.propertyname} </span>
+    );
+
     return (
-      <Row>
-        <Col md="9">
-          <h2>
-            {crowdsale.name} <span className="badge badge-secondary">{`(#${crowdsale.propertyid})`}</span>
-          </h2>
-          <div>
-            <AssetDetail>
-              {crowdsale.data}
-            </AssetDetail>
-          </div>
-          <div>
-            <AssetDetail>
-              <span>For more details visit:</span>
-              <Link
-                to={{
-                  pathname: crowdsale.url,
-                }}
-                target="_blank"
-              >
-                {crowdsale.url}
-              </Link>
-            </AssetDetail>
-          </div>
-          <div>
-            <AssetDetail>
-              <span>Issuance Address:</span>
-              <Link
-                to={{
-                  pathname: `/address/${crowdsale.issuer}`,
-                }}
-                onClick={() => this.props.changeRoute(`/address/${crowdsale.issuer}`)}
-              >
-                {crowdsale.issuer}
-              </Link>
-            </AssetDetail>
-          </div>
-          <div>
-            <AssetDetail>
-              <p>Share this page:
-                <Link
-                  to={{
-                    pathname: `https://www.facebook.com/sharer/sharer.php?u=https://www.omniwallet.org/assets/details/${crowdsale.propertyid}`,
-                  }}
-                  target="_blank"
-                >
-                  <FacebookIcon size={32}/>
-                </Link>
-                <Link
-                  to={{
-                    pathname: `https://plus.google.com/share?url=https://www.omniwallet.org/assets/details/${crowdsale.propertyid}`,
-                  }}
-                  target="_blank"
-                >
-                  <GPlusIcon size={32}/>
-                </Link>
-                <Link
-                  to={{
-                    pathname: `https://twitter.com/home?status=https://www.omniwallet.org/assets/details/${crowdsale.propertyid}`,
-                  }}
-                  target="_blank"
-                >
-                  <TwitterIcon size={32}/>
-                </Link>
-                <Link
-                  to={{
-                    pathname: 'https://www.linkedin.com/shareArticle?mini=true&amp;url=https://www.omniwallet.org/assets/details/' + crowdsale.propertyid + '&amp;title=Checkout%20this%20Crowdsale!&amp;summary=&amp;source=',
-                  }}
-                  target="_blank"
-                >
-                  <LinkedinIcon size={32}/>
-                </Link>
-              </p>
-            </AssetDetail>
-          </div>
-          <div>
-            <h2>
-              Asset History
-              <small className="text-muted">
-                ({detail.total}) transactions
-              </small>
-            </h2>
-            <Table striped>
-              <tbody>
-              {(detail.transactions || []).map((tx) => (
-                <tr>
-                  <td>tx</td>
-                </tr>
-              ))}
-              </tbody>
-            </Table>
-          </div>
-        </Col>
-        <Col md="3">
-          <Row>
-            <StyledCard>
-              <CardBody>
-                <CardTitle className="card-title">Active Crowdsale</CardTitle>
-                <span>Time Until Closing:</span>
-              </CardBody>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  Tokens already bought by participants
-                  <h2>
-                    <span className="badge badge-secondary">
-                      <SanitizedFormattedNumber value={crowdsale.tokensissued}/>
-                    </span>
-                  </h2>
-                </li>
-                <li className="list-group-item">
-                  Tokens created for the issuer (100%)
-                  <h2>
-                    <span className="badge badge-secondary">
-                      <SanitizedFormattedNumber value={(crowdsale.totaltokens * crowdsale.percenttoissuer / 100)}/>
-                    </span>
-                  </h2>
-                </li>
-                <li className="list-group-item">
-                  Current early bird bonus
-                  <h2>
-                    <span className="badge badge-secondary">
-                      +<SanitizedFormattedNumber value={crowdsale.earlybonus}/>%
-                    </span>
-                  </h2>
-                </li>
-              </ul>
-              <CardBody>
-                <CardTitle>Get some tokens!</CardTitle>
-                <Button color="primary" size="lg" disabled>Participate</Button>
-                <CardText className="card-text">You need to login or create a wallet to participate</CardText>
-              </CardBody>
-            </StyledCard>
+      <Container fluid className="mt-3">
+        <Row>
+          <Row noGutters>
+            <Col>
+              <h2>
+                {crowdsale.name} <span className="badge badge-secondary">{`(#${crowdsale.propertyid})`}</span>
+              </h2>
+            </Col>
           </Row>
-        </Col>
-      </Row>
+          <Row className="w-100">
+            <Col sm="12" md="9">
+              <StyledDivContent>
+                <AssetDetail>
+                  <StyledSpan>
+                    {crowdsale.data}
+                  </StyledSpan>
+                </AssetDetail>
+              </StyledDivContent>
+              <StyledDivContent>
+                <AssetDetail>
+                  <span>For more details visit:</span>&nbsp;
+                  <Link
+                    className="d-block d-lg-inline-block"
+                    to={{
+                      pathname: crowdsale.url,
+                    }}
+                    target="_blank"
+                  >
+                    {crowdsale.url}
+                  </Link>
+                </AssetDetail>
+              </StyledDivContent>
+              <StyledDivContent>
+                <AssetDetail>
+                  <span>Issuance Address:</span>&nbsp;
+                  <Link
+                    className="d-block d-lg-inline-block"
+                    to={{
+                      pathname: `/address/${crowdsale.issuer}`,
+                    }}
+                    onClick={() => this.props.changeRoute(`/address/${crowdsale.issuer}`)}
+                  >
+                    {crowdsale.issuer}
+                  </Link>
+                </AssetDetail>
+              </StyledDivContent>
+              <StyledDivContent>
+                <AssetDetail>
+                Share this page:
+                  <Link
+                    to={{
+                      pathname: `https://www.facebook.com/sharer/sharer.php?u=https://www.omniwallet.org/assets/details/${crowdsale.propertyid}`,
+                    }}
+                    target="_blank"
+                  >
+                    <FacebookIcon size={32} />
+                  </Link>
+                  <Link
+                    to={{
+                      pathname: `https://plus.google.com/share?url=https://www.omniwallet.org/assets/details/${crowdsale.propertyid}`,
+                    }}
+                    target="_blank"
+                  >
+                    <GPlusIcon size={32} />
+                  </Link>
+                  <Link
+                    to={{
+                      pathname: `https://twitter.com/home?status=https://www.omniwallet.org/assets/details/${crowdsale.propertyid}`,
+                    }}
+                    target="_blank"
+                  >
+                    <TwitterIcon size={32} />
+                  </Link>
+                  <Link
+                    to={{
+                      pathname: `https://www.linkedin.com/shareArticle?mini=true&amp;url=https://www.omniwallet.org/assets/details/${crowdsale.propertyid}&amp;title=Checkout%20this%20Crowdsale!&amp;summary=&amp;source=`,
+                    }}
+                    target="_blank"
+                  >
+                    <LinkedinIcon size={32} />
+                  </Link>
+                </AssetDetail>
+              </StyledDivContent>
+              <div>
+                <h2>
+                Asset History <small className="text-muted">({detail.total}) transactions</small>
+                </h2>
+                <Table striped>
+                  <tbody>
+                    {(detail.transactions || []).map((tx, idx) => (
+                      <tr key={tx.txid.slice(0, 22).concat(idx)}>
+                        <td>
+                          <Row>
+                            <Col md="4">
+                              <Moment fromNow>{tx.blocktime * 1000}</Moment>
+                            </Col>
+                            <Col md="8">
+                              <TransactionLabel tx={tx} />
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col md="4">
+                              <span className="text-muted">
+                                <FormattedUnixDateTime datetime={tx.blocktime} />
+                              </span>
+                            </Col>
+                            <Col md="8">
+                              <Link
+                                to={{
+                                  pathname: `/address/${tx.sendingaddress}`,
+                                }}
+                                onClick={() => this.props.changeRoute(`/address/${tx.sendingaddress}`)}
+                              >
+                                {tx.sendingaddress}
+                              </Link>
+                            </Col>
+                          </Row>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </div>
+            </Col>
+            <Col sm="12" md="3">
+              <Row>
+                <StyledCard color="info">
+                  <CardBody>
+                    <CardTitle className="card-title">Active Crowdsale</CardTitle>
+                    <span>Time Until Closing:</span>
+                    <Moment diff="2015-04-19" unit="years">{Date()}</Moment>
+                    <Moment diff="2015-04-19" unit="days">{Date()}</Moment>
+                  </CardBody>
+                  <ListGroup className="list-group-flush" color="info">
+                    <ListGroupItem>
+                    Tokens already bought by participants
+                      <h2>
+                        <span className="badge badge-secondary">
+                          <SanitizedFormattedNumber value={crowdsale.tokensissued} />
+                        </span>
+                      </h2>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                      <h5>Tokens created for the issuer ({crowdsale.percenttoissuer}%)</h5>
+                      <h2>
+                        <span className="badge badge-secondary">
+                          <SanitizedFormattedNumber value={(crowdsale.totaltokens * crowdsale.percenttoissuer / 100)} />
+                        </span>
+                      </h2>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                    Current early bird bonus
+                      <h2>
+                        <span className="badge badge-secondary">
+                      +<SanitizedFormattedNumber value={crowdsale.earlybonus} />%
+                        </span>
+                      </h2>
+                    </ListGroupItem>
+                  </ListGroup>
+                  <CardBody>
+                    <CardTitle>Get some tokens!</CardTitle>
+                    <Button color="primary" size="lg" disabled>Participate</Button>
+                    <CardText className="card-text">You need to login or create a wallet to participate</CardText>
+                  </CardBody>
+                </StyledCard>
+              </Row>
+            </Col>
+          </Row>
+        </Row>
+      </Container>
     );
   }
 }
@@ -206,6 +267,7 @@ CrowdsaleDetail.propTypes = {
   properties: PropTypes.func.isRequired,
   getPropertyDeep: PropTypes.func.isRequired,
   getCrowdsaleTransactions: PropTypes.func.isRequired,
+  crowdsaledetail: PropTypes.object,
 };
 
 const mapStateToProps = createStructuredSelector({
