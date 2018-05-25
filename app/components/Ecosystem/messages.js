d4af7b61a3e6846f175ce1db3c0da33d7c6b69fc
germanf <ferreyra.ger@gmail.com>
2018-05-25 08:56:48 -0300

    + Ecosystem Dropdown componentization

---
+++ app/components/Ecosystem/messages.js
@@ -0,0 +1,19 @@
+/*
+ * Ecosystem Messages
+ *
+ * This contains all the text for the Ecosystem component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  ecosystem: {
+    prod: {
+      id: 'app.containers.Crowdsales.ecosystem.prod',
+      defaultMessage: 'Production',
+    },
+    test: {
+      id: 'app.containers.Crowdsales.ecosystem.test',
+      defaultMessage: 'Test',
+    },
+  },
+});
