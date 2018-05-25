619a8cf616daece392008912abecb42768065731
germanf <ferreyra.ger@gmail.com>
2018-05-25 13:03:38 -0300

    + added tooltip to FormattedDatetime Component to advise when datetime is UTC, by default all datetimes are handled UTC

--- app/components/FormattedDateTime/FormattedDateTime.js
+++ app/components/FormattedDateTime/FormattedDateTime.js
@@ -12,23 +12,29 @@ import { UncontrolledTooltip } from 'reactstrap';
 
 import messages from './messages';
 
-const FormattedDateTime = ({ datetime, isUTC = true, key = (Date.now()) }) => (
-  <span>
-    <FormattedMessage
-      {...messages.datetime}
-      values={{
-        date: <FormattedDate value={datetime}/>,
-        hour: <FormattedTime value={datetime} hour="numeric" minute="numeric" second="numeric"/>,
-      }}
-    />
-    <InformationIcon color="gray" className="ml-1" id={key}/>
-    {isUTC &&
-    <UncontrolledTooltip placement="top" target={key}>
-      <FormattedMessage {...messages.utc} />
-    </UncontrolledTooltip>
-    }
+const FormattedDateTime = ({ datetime, isUTC = true, key = (Date.now()) }) => {
+  const id = `id_${key}`;
+  
+  return (
+    <span>
+      <FormattedMessage
+        {...messages.datetime}
+        values={{
+          date: <FormattedDate value={datetime}/>,
+          hour: <FormattedTime value={datetime} hour="numeric" minute="numeric" second="numeric"/>,
+        }}
+      />
+      {isUTC &&
+        <span>
+          <InformationIcon color="gray" className="ml-1" id={id} />
+          <UncontrolledTooltip placement="top" target={id}>
+            <FormattedMessage {...messages.utc} />
+          </UncontrolledTooltip>
+        </span>
+      }
   </span>
-);
+  );
+};
 
 FormattedDateTime.propTypes = {
   datetime: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.instanceOf(Date)]),
