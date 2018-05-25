6f81caa70e23fe58f5b7f456e518968bbbd3ce49
germanf <ferreyra.ger@gmail.com>
2018-05-25 12:12:35 -0300

    + added tooltip to FormattedDateTime Component, default behavior is handle datetimes as UTC

--- app/components/FormattedDateTime/FormattedDateTime.js
+++ app/components/FormattedDateTime/FormattedDateTime.js
@@ -6,17 +6,28 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage, FormattedDate, FormattedTime } from 'react-intl';
+import { FormattedDate, FormattedMessage, FormattedTime } from 'react-intl';
+import InformationIcon from 'react-icons/lib/io/informatcircled';
+import { UncontrolledTooltip } from 'reactstrap';
+
 import messages from './messages';
 
-const FormattedDateTime = ({ datetime }) => (
-  <FormattedMessage
-    {...messages.datetime}
-    values={{
-      date: <FormattedDate value={datetime} />,
-      hour: <FormattedTime value={datetime} hour="numeric" minute="numeric" second="numeric" />,
-    }}
-  />
+const FormattedDateTime = ({ datetime, isUTC = true, key = (Date.now()) }) => (
+  <span>
+    <FormattedMessage
+      {...messages.datetime}
+      values={{
+        date: <FormattedDate value={datetime}/>,
+        hour: <FormattedTime value={datetime} hour="numeric" minute="numeric" second="numeric"/>,
+      }}
+    />
+    <InformationIcon color="gray" className="ml-1" id={key}/>
+    {isUTC &&
+    <UncontrolledTooltip placement="top" target={key}>
+      <FormattedMessage {...messages.utc} />
+    </UncontrolledTooltip>
+    }
+  </span>
 );
 
 FormattedDateTime.propTypes = {
