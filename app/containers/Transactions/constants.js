7706683bc0e921750677db074c9a2604df82fb7c
germanf <ferreyra.ger@gmail.com>
2018-06-18 20:36:25 -0300

    + added SET_TRANSACTION_TYPE constant

--- app/containers/Transactions/constants.js
+++ app/containers/Transactions/constants.js
@@ -8,4 +8,5 @@ export const LOAD_TRANSACTIONS = 'omniexplorer/App/LOAD_TRANSACTIONS';
 export const LOAD_TRANSACTIONS_SUCCESS = 'omniexplorer/App/LOAD_TRANSACTIONS_SUCCESS';
 export const LOAD_TRANSACTIONS_ERROR = 'omniexplorer/App/LOAD_TRANSACTIONS_ERROR';
 export const SET_PAGE = 'omniexplorer/App/SET_PAGE';
+export const SET_TRANSACTION_TYPE = 'omniexplorer/App/SET_TRANSACTION_TYPE';
 export const CONFIRMATIONS = 100;
