6f63331d0528c37999f1340e7cc4c1921c59715f
germanf <ferreyra.ger@gmail.com>
2018-06-19 17:07:37 -0300

    + added option to show/hide seconds in FormattedDateTime components

--- app/components/FormattedDateTime/FormattedUnixDateTime.js
+++ app/components/FormattedDateTime/FormattedUnixDateTime.js
@@ -8,14 +8,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FormattedDateTime from './FormattedDateTime';
 
-const FormattedUnixDateTime = ({ datetime }) => (
+const FormattedUnixDateTime = ({ datetime, useSeconds }) => (
   ((new Date(datetime * 1000)).getTime() > 0) ?
-    (<FormattedDateTime datetime={datetime * 1000} />) :
+    (<FormattedDateTime datetime={datetime * 1000} useSeconds={useSeconds} />) :
     (<span> invalid date</span>)
 );
 
 FormattedUnixDateTime.propTypes = {
   datetime: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.instanceOf(Date)]),
+  useSeconds: PropTypes.bool,
 };
 
 export default FormattedUnixDateTime;
