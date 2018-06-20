8c58ac542977ed2e47cc7d8c7cae2ec17021356e
germanf <ferreyra.ger@gmail.com>
2018-06-20 16:11:57 -0300

    + improvement of decimal support in SanitizedFormattedNumber component

--- app/components/SanitizedFormattedNumber/index.jsx
+++ app/components/SanitizedFormattedNumber/index.jsx
@@ -12,14 +12,28 @@ function SanitizedFormattedNumber(props) {
 // eslint-disable-next-line no-restricted-globals
   const isNumeric = (data) => (!isNaN(parseFloat(data)) && isFinite(data) && data.constructor !== Array);
   const value = parseFloat(props.value, 10).toString();
-  const fractionDigits = props.fractionDigits || value.slice(value.indexOf('.') + 1).length;
-
-  const number = (isNumeric(props.value) ?
-    (<FormattedNumber
-      value={value}
-      minimunFractionDigits={fractionDigits}
-      maximumFractionDigits={fractionDigits}
-    />) :
+  const hasDecimals = props.value.indexOf && props.value.indexOf('.') !== -1;
+  const decimaldigits = (
+    (hasDecimals && value % 1 === 0) || props.forceDecimals ?
+      `.${new Array(3).join('0')}` :
+      ''
+  );
+  const fractionDigits = (
+    decimaldigits ?
+      3 :
+      props.fractionDigits || (props.value < 8 ? value.slice(props.value.indexOf('.') + 1).length : 8)
+  );
+  const number = (isNumeric(value) ?
+    (
+      <span>
+        <FormattedNumber
+          value={value}
+          minimunFractionDigits={fractionDigits}
+          maximumFractionDigits={fractionDigits}
+        />
+        {!!fractionDigits && decimaldigits}
+      </span>
+    ) :
     <span>{DEFAULT_NOT_NUMBER}</span>
   );
 
