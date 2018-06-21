c834e8f358b9a169e59a393195493f969afe3808
germanf <ferreyra.ger@gmail.com>
2018-06-21 18:44:19 -0300

    + padding zeros to 2 in decimal places by default

--- app/components/SanitizedFormattedNumber/index.jsx
+++ app/components/SanitizedFormattedNumber/index.jsx
@@ -11,13 +11,20 @@ import { DEFAULT_NOT_NUMBER } from 'containers/App/constants';
 function SanitizedFormattedNumber(props) {
 // eslint-disable-next-line no-restricted-globals
   const isNumeric = (data) => (!isNaN(parseFloat(data)) && isFinite(data) && data.constructor !== Array);
-  const value = parseFloat(props.value, 10).toString();
+  let value = parseFloat(props.value, 10).toString();
   const hasDecimals = props.value.indexOf && props.value.indexOf('.') !== -1;
-  const decimaldigits = (
-    (hasDecimals && value % 1 === 0) || props.forceDecimals ?
-      `.${new Array(3).join('0')}` :
-      ''
-  );
+
+  let decimaldigits;
+  if (hasDecimals || props.forceDecimals) {
+    if (value % 1 === 0) {
+      decimaldigits = `.${new Array(3).join('0')}`;
+    } else {
+      value = value.slice(value.indexOf('.') + 1).length < 2 ? `${value}0` : value;
+    }
+  } else {
+    decimaldigits = '';
+  }
+
   const fractionDigits = (
     decimaldigits ?
       3 :
