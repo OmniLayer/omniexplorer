788914122ffead86c40016e56555bf3df5d826a5
germanf <ferreyra.ger@gmail.com>
2018-07-26 12:19:05 -0300

    + fix prettfier & lint warnings in `Transaction Detail` component files

--- app/containers/TransactionDetail/constants.js
+++ app/containers/TransactionDetail/constants.js
@@ -5,5 +5,6 @@
  */
 
 export const LOAD_TRANSACTION = 'omniexplorer/App/LOAD_TRANSACTION';
-export const LOAD_TRANSACTION_SUCCESS = 'omniexplorer/App/LOAD_TRANSACTION_SUCCESS';
+export const LOAD_TRANSACTION_SUCCESS =
+  'omniexplorer/App/LOAD_TRANSACTION_SUCCESS';
 export const LOAD_TRANSACTION_ERROR = 'omniexplorer/App/LOAD_TRANSACTION_ERROR';
