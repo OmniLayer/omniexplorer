bfa1c833fdb4b7397b2ec9d2b89365020ec259a0
germanf <ferreyra.ger@gmail.com>
2018-07-26 12:22:06 -0300

    + fix prettfier & lint warnings in `FormattedDateTime` component files

--- app/components/FormattedDateTime/FormattedDateTime.js
+++ app/components/FormattedDateTime/FormattedDateTime.js
@@ -10,10 +10,14 @@ import { FormattedDate, FormattedMessage, FormattedTime } from 'react-intl';
 
 import messages from './messages';
 
-const FormattedDateTime = ({ datetime, key = (Date.now()), useSeconds = true }) => {
-// eslint-disable-next-line no-unused-vars
+const FormattedDateTime = ({
+  datetime,
+  key = Date.now(),
+  useSeconds = true,
+}) => {
+  // eslint-disable-next-line no-unused-vars
   const id = `id_${key}`;
-  const seconds = (useSeconds ? { second: 'numeric' } : null);
+  const seconds = useSeconds ? { second: 'numeric' } : null;
 
   return (
     <span>
@@ -21,7 +25,14 @@ const FormattedDateTime = ({ datetime, key = (Date.now()), useSeconds = true }) 
         {...messages.datetime}
         values={{
           date: <FormattedDate value={datetime} />,
-          hour: <FormattedTime value={datetime} hour="numeric" minute="numeric" {...seconds} />,
+          hour: (
+            <FormattedTime
+              value={datetime}
+              hour="numeric"
+              minute="numeric"
+              {...seconds}
+            />
+          ),
         }}
       />
     </span>
@@ -29,7 +40,11 @@ const FormattedDateTime = ({ datetime, key = (Date.now()), useSeconds = true }) 
 };
 
 FormattedDateTime.propTypes = {
-  datetime: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.instanceOf(Date)]),
+  datetime: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.instanceOf(Date),
+  ]),
   key: PropTypes.any,
   useSeconds: PropTypes.bool,
 };
--- app/components/FormattedDateTime/FormattedUnixDateTime.js
+++ app/components/FormattedDateTime/FormattedUnixDateTime.js
@@ -8,14 +8,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FormattedDateTime from './FormattedDateTime';
 
-const FormattedUnixDateTime = ({ datetime, useSeconds }) => (
-  ((new Date(datetime * 1000)).getTime() > 0) ?
-    (<FormattedDateTime datetime={datetime * 1000} useSeconds={useSeconds} />) :
-    (<span> invalid date</span>)
-);
+const FormattedUnixDateTime = ({ datetime, useSeconds }) =>
+  new Date(datetime * 1000).getTime() > 0 ? (
+    <FormattedDateTime datetime={datetime * 1000} useSeconds={useSeconds} />
+  ) : (
+    <span> invalid date</span>
+  );
 
 FormattedUnixDateTime.propTypes = {
-  datetime: PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.instanceOf(Date)]),
+  datetime: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+    PropTypes.instanceOf(Date),
+  ]),
   useSeconds: PropTypes.bool,
 };
 
