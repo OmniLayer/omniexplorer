539f9a04394113a02b0ac2d511c0f22a5e56c400
Adam <adam@mastercoin.org>
2018-08-03 12:28:41 -0500

    fix null value

--- app/components/SanitizedFormattedNumber/index.jsx
+++ app/components/SanitizedFormattedNumber/index.jsx
@@ -11,7 +11,11 @@ import { DEFAULT_NOT_NUMBER } from 'containers/App/constants';
 function SanitizedFormattedNumber(props) {
 // eslint-disable-next-line no-restricted-globals
   const isNumeric = (data) => (!isNaN(parseFloat(data)) && isFinite(data) && data.constructor !== Array);
-  const value = props.value.toFixed ? props.value.toFixed(8) : parseFloat(props.value, 10).toString();
+  if (props.value == undefined) {
+    return;
+  } else {
+    const value = props.value.toFixed ? props.value.toFixed(8) : parseFloat(props.value, 10).toString();
+  }
   const hasDecimals = props.value.indexOf && props.value.indexOf('.') !== -1;
 
   let decimaldigits;
