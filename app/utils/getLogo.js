f8f7b7a1a1c66b1b068ff279cc2e559e86742399
germanf <ferreyra.ger@gmail.com>
2018-08-20 17:21:41 -0500

    + split getLogo helper in getLogoPath & getLogo

--- app/utils/getLogo.js
+++ app/utils/getLogo.js
@@ -1,14 +1,21 @@
 /* eslint-disable global-require */
-export default id => {
+
+export const getLogoPath = (id, flags) => {
   let logo;
   try {
-    logo = require(`images/token${id}.png`);
+    if (flags && (flags.duplicate || flags.scam)) {
+      logo = 'images/tokenwarn.png';
+    } else {
+      logo = `images/token${id}.png`;
+    }
   } catch (e) {
     if (id > 2147483650) {
-      logo = require('images/test-dev-icon.png');
+      logo = 'images/test-dev-icon.png';
     } else {
-      logo = require('images/tokendefault.png');
+      logo = 'images/tokendefault.png';
     }
   }
   return logo;
 };
+
+export default (id, flags) => require(getLogoPath(id, flags));
