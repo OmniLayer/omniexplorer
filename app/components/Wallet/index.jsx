bd5273d239007a19c4f01a5f82498fd6b386c8a5
germanf <ferreyra.ger@gmail.com>
2018-09-06 19:24:35 -0300

    + splitted properties into flagged and non flagged + added collapse to show/hide flagged properties + #refactoring Wallet stateless function promoted to PureComponent

--- app/components/Wallet/index.jsx
+++ app/components/Wallet/index.jsx
@@ -6,8 +6,11 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { routeActions } from 'redux-simple-router';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 import styled from 'styled-components';
-import { Col, Container, Row, Table, Collapse } from 'reactstrap';
+import { Col, Container, Row, Table, UncontrolledCollapse } from 'reactstrap';
 import QRCode from 'qrcode.react';
 import isEmpty from 'lodash/isEmpty';
 import Token from 'components/Token';
@@ -27,123 +30,131 @@ const StyledTH = styled.th`
   border: none !important;
 `;
 
-const Wallet = props => {
+class Wallet extends React.PureComponent {
   // eslint-disable-line react/prefer-stateless-function
-  const flaggedProps = [];
-  const dontFlaggedProps = [];
+  constructor(props) {
+    super(props);
+  }
 
-  let collapseFlagged = false;
-  const toggleFlagged = () => (collapseFlagged = !collapseFlagged);
+  render() {
+    const flaggedProps = (this.props.address.balance || []).filter(
+      balance => !isEmpty(balance.propertyinfo.flags, true),
+    );
 
-  (props.address.balance || []).forEach(balance => {
-    if (!balance.propertyinfo || isEmpty(balance.propertyinfo.flags, true)) {
-      dontFlaggedProps.push(balance);
-    } else {
-      flaggedProps.push(balance);
-    }
-  });
+    const nonFlaggedProps = (this.props.address.balance || []).filter(balance =>
+      isEmpty(balance.propertyinfo.flags, true),
+    );
 
-  return (
-    <Container fluid>
-      <DetailRow>
-        <Col className="col-auto mx-auto" sm="3">
-          <QRCode value={props.addr} />
-        </Col>
-        <Col sm>
-          <Table responsive className="table-profile">
-            <thead>
-              <tr>
-                <StyledTH />
-                <StyledTH>
-                  <h4>
-                    <span className="d-block" id="laddress">
-                      {props.addr}
-                    </span>
-                  </h4>
-                  {props.extra}
-                </StyledTH>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="highlight">
-                <StyledTD className="field font-weight-bold">
-                  <Table className="table" style={{ marginBottom: '5px' }}>
-                    <thead>
-                      <tr>
-                        <StyledTH>Balances</StyledTH>
-                      </tr>
-                    </thead>
-                  </Table>
-                </StyledTD>
-                <StyledTD>
-                  <Table className="table" style={{ marginBottom: '5px' }}>
-                    <thead>
-                      <tr>
-                        <StyledTH />
-                        <StyledTH>ID</StyledTH>
-                        <StyledTH>Name</StyledTH>
-                        <StyledTH className="text-right">
-                          Reserved Balance
-                        </StyledTH>
-                        <StyledTH className="text-right">
-                          Available Balance
-                        </StyledTH>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {dontFlaggedProps.map(balance => (
-                        <Token {...balance} key={balance.id} />
-                      ))}
-                    </tbody>
-                  </Table>
-                </StyledTD>
-              </tr>
-            </tbody>
-          </Table>
-          <a
-            href="#collapseFlagged"
-            color="primary"
-            onClick={toggleFlagged}
-            style={{ marginBottom: '1rem' }}
-          >
-            Show flagged properties...
-          </a>
-          <Collapse isOpen={collapseFlagged}>
-            <Table>
+    const hasFlagged = !!flaggedProps.length;
+
+    return (
+      <Container fluid>
+        <DetailRow>
+          <Col className="col-auto mx-auto" sm="3">
+            <QRCode value={this.props.addr} />
+          </Col>
+          <Col sm>
+            <Table responsive className="table-profile">
+              <thead>
+                <tr>
+                  <StyledTH />
+                  <StyledTH>
+                    <h4>
+                      <span className="d-block" id="laddress">
+                        {this.props.addr}
+                      </span>
+                    </h4>
+                    {this.props.extra}
+                  </StyledTH>
+                </tr>
+              </thead>
               <tbody>
                 <tr className="highlight">
-                  <StyledTD />
+                  <StyledTD className="field font-weight-bold">
+                    <Table className="table" style={{ marginBottom: '5px' }}>
+                      <thead>
+                        <tr>
+                          <StyledTH>
+                            Balances
+                            <br />
+                            <a
+                              href="#togglerFlagged"
+                              className="text-info small"
+                              id="togglerFlagged"
+                              style={{ marginBottom: '1rem' }}
+                              disabled={hasFlagged}
+                            >
+                              Show flagged...
+                            </a>
+                          </StyledTH>
+                        </tr>
+                      </thead>
+                    </Table>
+                  </StyledTD>
                   <StyledTD>
                     <Table className="table" style={{ marginBottom: '5px' }}>
                       <thead>
                         <tr>
                           <StyledTH />
-                          <StyledTH rowspan="6" />
+                          <StyledTH>ID</StyledTH>
+                          <StyledTH>Name</StyledTH>
+                          <StyledTH className="text-right">
+                            Reserved Balance
+                          </StyledTH>
+                          <StyledTH className="text-right">
+                            Available Balance
+                          </StyledTH>
                         </tr>
                       </thead>
                       <tbody>
-                        {flaggedProps.map(balance => (
+                        {nonFlaggedProps.map(balance => (
                           <Token {...balance} key={balance.id} />
                         ))}
                       </tbody>
                     </Table>
+                    <UncontrolledCollapse toggler="#togglerFlagged">
+                      <Table className="table" style={{ marginBottom: '5px' }}>
+                        <thead>
+                          <tr>
+                            <StyledTH />
+                            <StyledTH colSpan="4" className="text-center">
+                              Flagged properties
+                            </StyledTH>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {flaggedProps.map(balance => (
+                            <Token {...balance} key={balance.id} />
+                          ))}
+                        </tbody>
+                      </Table>
+                    </UncontrolledCollapse>
                   </StyledTD>
-                </tr>
-                <tr className="divider">
-                  <td colSpan="2" />
                 </tr>
               </tbody>
             </Table>
-          </Collapse>
-        </Col>
-      </DetailRow>
-    </Container>
-  );
-};
+          </Col>
+        </DetailRow>
+      </Container>
+    );
+  }
+}
 
 Wallet.propTypes = {
   address: PropTypes.object.isRequired,
   addr: PropTypes.string.isRequired,
 };
 
-export default Wallet;
+function mapDispatchToProps(dispatch) {
+  return {
+    changeRoute: url => dispatch(routeActions.push(url)),
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  null,
+  mapDispatchToProps,
+);
+
+export default compose(withConnect)(Wallet);
