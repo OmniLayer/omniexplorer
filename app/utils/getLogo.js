ec6850c26a6a61ccfaad8daca0970c78f8ab0f3c
germanf <ferreyra.ger@gmail.com>
2018-09-10 17:33:57 -0300

    + #refactoring logic to resolve logo based on flags

--- app/utils/getLogo.js
+++ app/utils/getLogo.js
@@ -1,4 +1,5 @@
 /* eslint-disable global-require,camelcase */
+import isEmpty from 'lodash/isEmpty';
 
 export default (id, propertyinfo = {}) => {
   let logo;
@@ -7,14 +8,24 @@ export default (id, propertyinfo = {}) => {
   try {
     if (type_int === 4) {
       logo = require('images/sendall.png');
-    } else if (flags && (flags.duplicate || flags.scam || flags.replaced)) {
-      logo = require('images/tokenwarn.png');
+    } else if (!isEmpty(flags, true)) {
+      if (flags.duplicate) {
+        logo = require('images/tokenduplicate.png');
+      } else if (flags.scam) {
+        logo = require('images/tokenscam.png');
+      } else if (flags.replaced) {
+        logo = require('images/tokenreplaced.png');
+      } else if (flags.registered) {
+        logo = require(`images/token${id}.png`);
+      }
     } else {
       logo = require(`images/token${id}.png`);
     }
   } catch (e) {
     if (id > 2147483650) {
       logo = require('images/test-dev-icon.png');
+    } else if (flags && flags.registered) {
+      logo = require('images/tokenregistered.png');
     } else {
       logo = require('images/tokendefault.png');
     }
