bf2c46e33488b59b0a3601b8a9dd7e2e06e1e33e
germanf <ferreyra.ger@gmail.com>
2018-11-27 13:43:31 -0300

    + added `BlockFullList` container

---
+++ app/containers/BlockFullList/index.jsx
@@ -0,0 +1,40 @@
+/**
+ *
+ * BlockFullList
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import { compose } from 'redux';
+
+import messages from './messages';
+
+export class BlockFullList extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <FormattedMessage {...messages.header} />
+      </div>
+    );
+  }
+}
+
+BlockFullList.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(null, mapDispatchToProps);
+
+export default compose(
+  withConnect,
+)(BlockFullList);
---
+++ app/containers/BlockFullList/messages.js
@@ -0,0 +1,13 @@
+/*
+ * BlockFullList Messages
+ *
+ * This contains all the text for the BlockFullList component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.containers.BlockFullList.header',
+    defaultMessage: 'This is BlockFullList container !',
+  },
+});
