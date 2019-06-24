cce34d189920a91c8ec416460a2356ece0a08a16
germanf <ferreyra.ger@gmail.com>
2019-06-24 20:25:47 -0300

    + added client side pagination to block transactions

--- app/containers/BlockDetail/index.jsx
+++ app/containers/BlockDetail/index.jsx
@@ -37,11 +37,7 @@ import reducer from './reducer';
 import { loadBlock } from './actions';
 import sagaBlock from './saga';
 import messages from './messages';
-import {
-  ALL_BLOCK_TRANSACTIONS,
-  VALID_BLOCK_TRANSACTIONS,
-  INVALID_BLOCK_TRANSACTIONS,
-} from './constants';
+import { ALL_BLOCK_TRANSACTIONS, INVALID_BLOCK_TRANSACTIONS, VALID_BLOCK_TRANSACTIONS } from './constants';
 
 import './blockdetail.scss';
 
@@ -56,49 +52,74 @@ const StyledContainer = styled(ContainerBase).attrs({
   }
 `;
 
-export class BlockDetail extends React.PureComponent {
+export class BlockDetail extends React.Component {
   // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
-
+    
     this.block = props.match.params.block;
     this.state = {
       showTxType: ALL_BLOCK_TRANSACTIONS,
+      data: [],
+      currentData: [],
+      pageCount: 0,
+      currentPage: 1,
     };
+    
     this.transactions = null;
-
+    
     this.onShowInvalidTxs = this.onShowInvalidTxs.bind(this);
     this.getTransactions = this.getTransactions.bind(this);
+    this.handlePageClick = this.handlePageClick.bind(this);
   }
-
+  
   getTransactions() {
     console.log('call getTransactions');
     const { block } = this.props.blockdetail;
-
+    
     if (!this.transactions) {
+      this.setState({
+        currentData: block.transactions.slice(0, 10),
+        pageCount: Math.ceil(block.transactions.length / 10),
+      });
       this.transactions = {
         [ALL_BLOCK_TRANSACTIONS]: block.transactions,
         [VALID_BLOCK_TRANSACTIONS]: block.transactions.filter(x => x.valid),
         [INVALID_BLOCK_TRANSACTIONS]: block.transactions.filter(x => !x.valid),
       };
     }
-
+    
     const txs = this.transactions[this.state.showTxType];
     return txs;
   }
-
+  
   onShowInvalidTxs(showTxType) {
     this.setState({ showTxType });
   }
-
+  
   componentDidMount() {
     console.log('block detail did mount');
     this.props.loadBlock(this.block);
   }
-
+  
+  handlePageClick = page => {
+    const txs = this.getTransactions();
+    // let page = Math.ceil(selected * this.props.perPage);
+    
+    this.setState({
+      currentPage: page,
+      currentData: txs.slice(page, page + 10),
+    });
+    
+    // txs = this.getTransactions();
+    // this.setState({
+    //   data: txs,
+    //   pageCount: Math.ceil(txs.length / 10),
+    // });
+  };
+  
   render() {
     console.log('block detail render');
-
     const statusLoading = (!this.props || !this.props.status || !this.props.status.last_block);
     if (this.props.blockdetail.loading || statusLoading) {
       return (
@@ -107,20 +128,20 @@ export class BlockDetail extends React.PureComponent {
         </Container>
       );
     }
-
+    // this.handlePageClick(this.state.currentPage);
+    const txs = this.getTransactions();
     const { last_block: lastBlock } = this.props.status;
     const { block } = this.props.blockdetail;
     const { confirmations } = (block.transactions || []).find(
       tx => tx.valid,
     ) || { confirmations: 'invalid' };
-
+    
     let content;
-    let txs = [];
     let hasInvalid = false;
-
+    
     const getItemKey = (blockItem, idx) => blockItem.blockhash.slice(0, 22).concat(idx);
-    const hashLink = txid => `/tx/${txid}`;
-
+    // const hashLink = txid => `/tx/${txid}`;
+    
     if (this.block < FIRST_BLOCK || !block.transactions) {
       const errMsg = `Block ${this.block} not found`;
       content = (
@@ -142,22 +163,27 @@ export class BlockDetail extends React.PureComponent {
         </h3>
       );
     } else {
-      txs = this.getTransactions();
       hasInvalid = !isEmpty(this.transactions[INVALID_BLOCK_TRANSACTIONS]);
-
+      
       content = (
-        <List
-          {...block}
-          items={txs}
-          inner={Transaction}
-          getItemKey={getItemKey}
-          hashLink={hashLink}
-        />
+        <div>
+          <List
+            {...block}
+            usePagination={true}
+            pageCount={this.state.pageCount}
+            currentPage={this.state.currentPage}
+            onSetPage={this.handlePageClick}
+            items={this.state.currentData}
+            inner={Transaction}
+            getItemKey={getItemKey}
+            hashLink={(key) => `#${key}`}
+          />
+        </div>
       );
     }
     const footer = <FooterLinks unconfirmed blocklist/>;
-    const dropdownToggle = ()=>{
-      switch (this.state.showTxType) {
+    const dropdownToggle = () => {
+      switch(this.state.showTxType) {
         case ALL_BLOCK_TRANSACTIONS:
           return 'All Transactions';
         case VALID_BLOCK_TRANSACTIONS:
@@ -167,7 +193,7 @@ export class BlockDetail extends React.PureComponent {
         default:
           return 'Transactions';
       }
-    }
+    };
     // const pluralize = hasInvalid > 1 ? 's' : '';
     const dropdown = <UncontrolledDropdown className="float-md-right">
       <DropdownToggle caret>
@@ -179,9 +205,9 @@ export class BlockDetail extends React.PureComponent {
         <DropdownItem onClick={() => this.onShowInvalidTxs(INVALID_BLOCK_TRANSACTIONS)}>Show Invalid</DropdownItem>
       </DropdownMenu>
     </UncontrolledDropdown>;
-
+    
     const validInvalidTxs = hasInvalid ? dropdown : null;
-
+    
     return (
       <StyledContainer fluid>
         <ListHeader
@@ -214,11 +240,11 @@ export class BlockDetail extends React.PureComponent {
           <br/>
           {validInvalidTxs}
         </ListHeader>
+        {content}
         <BlockPagination
           block={this.block}
           latest={lastBlock}
         />
-        {content}
         {footer}
       </StyledContainer>
     );
