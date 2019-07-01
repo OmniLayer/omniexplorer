274efe3dadbfe7a27c559548914a198de261b32a
Alex R <42213681+alexeirbv@users.noreply.github.com>
2019-07-01 17:03:29 +0300

    Fix ip backend

--- server/middlewares/addDevMiddlewares.js
+++ server/middlewares/addDevMiddlewares.js
@@ -33,7 +33,7 @@ module.exports = function addDevMiddlewares(app, webpackConfig) {
   app.use(
     '/api',
     proxy({
-      target: 'http://144.76.73.140:4005/',
+      target: 'http://localhost:4005/',
       changeOrigin: true,
       logLevel: 'debug',
       pathRewrite: {
