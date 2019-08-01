0d953f7de104f3ab6fb480cb84e29ec1c480c143
germanf <ferreyra.ger@gmail.com>
2019-08-01 11:00:49 -0300

    + added `WrapperLink` component

---
+++ app/components/WrapperLink/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import WrapperLink from '../index';
+
+describe('<WrapperLink />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
