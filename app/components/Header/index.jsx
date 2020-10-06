8ba8e27b1313f956133cbcf62277e6878c2b4369
germanf <ferreyra.ger@gmail.com>
2020-10-06 17:18:07 -0300

    + refactoring Header to use new `Menu` component

--- app/components/Header/index.jsx
+++ app/components/Header/index.jsx
@@ -6,108 +6,41 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
 import styled from 'styled-components';
-import SearchBox from 'components/SearchBox';
+
+import Menu from 'components/Menu';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import NavigationBar from 'components/NavigationBar';
-
-import DarkModeToggle from 'components/DarkModeToggle';
-import MenuButton from 'components/MenuButton';
-
-// import { slide as Menu } from 'react-burger-menu';
-import { push as Menu } from 'react-burger-menu';
-// import Menu from 'react-burger-menu/lib/menus/slide';
-
-import {
-  Alert,
-  Collapse,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Nav,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  Button,
-  UncontrolledCollapse,
-  Card,
-  CardBody,
-} from 'reactstrap';
 import ServiceBlock from 'components/ServiceBlock';
-import getLocationPath, {getSufixURL} from 'utils/getLocationPath';
+import SearchBox from 'components/SearchBox';
 
 const IMG = styled.img`
   padding-bottom: 3px;
   padding-right: 9px;
 `;
 
-const StyledNavigationBar = styled(NavigationBar)`
-  padding-left: 3rem !important;
-  min-height: 3rem;
-`;
-
-const StyledNavItem = styled(NavItem)`
-  font-size: 16px;
-`;
-
-const StyledCollapse = styled(Collapse)`
-  font-size: 16px;
-`;
-
-class Header extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  toggle = (e)  => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-          <Navbar color="faded" dark>
-            <NavbarBrand href="/">
-              <IMG src="/favicon.png" alt="OMNIEXPLORER.INFO" />
-              OMNIEXPLORER.INFO
-            </NavbarBrand>
-          <DarkModeToggle />
-          <MenuButton/>
-          {/*<NavbarToggler onClick={this.toggle} />*/}
-          {/*asdñlkfasñlkdfj*/}
-        </Navbar>
-        <ServiceBlock />
-      </div>
-    );
-  }
+function Header(props) {
+  return (
+    <div id="app-header">
+      <NavigationBar>
+        <NavbarBrand href="/">
+          <IMG src="/favicon.png" alt="OmniExplorer.info" />
+          OmniExplorer.info
+        </NavbarBrand>
+        <div className="ml-auto w-50 d-flex">
+          <div className="w-100 ml-auto d-none-only-sm-down">
+            <SearchBox />
+          </div>
+          <Menu />
+        </div>
+        <div className="w-100 ml-auto d-block-only-sm-down">
+          <SearchBox className="w-100"/>
+        </div>
+      </NavigationBar>
+      <ServiceBlock />
+    </div>
+  );
 }
-
-// const oldnavbar = (
-//   <StyledNavigationBar fixed="top">
-//     <div className="d-flex">
-//       <NavbarBrand href="/">
-//         OmniExplorer.info
-//       </NavbarBrand>
-//       <DarkModeToggle />
-//       <div className="ml-auto w-50 d-flex">
-//         <div className="w-100 ml-auto d-none-only-sm-down">
-//           <SearchBox />
-//         </div>
-//         <NavbarToggler onClick={this.toggle} />
-//       </div>
-//     </div>
-//   </StyledNavigationBar>
-// );
 
 Navbar.propTypes = {
   light: PropTypes.bool,
@@ -119,18 +52,6 @@ Navbar.propTypes = {
   tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
 };
 
-NavbarToggler.propTypes = {
-  type: PropTypes.string,
-  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
-};
+Header.propTypes = {};
 
-
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
-
-const withConnect = connect(null, mapDispatchToProps);
-
-export default compose(withConnect, )(Header);
+export default Header;
