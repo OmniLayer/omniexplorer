95d5a4ced9ad344e4883d30a6efcb7a402fcc791
germanf <ferreyra.ger@gmail.com>
2021-06-07 17:07:55 -0300

    + avoid to generate new element id on each render

--- app/components/CopyToClipboard/index.js
+++ app/components/CopyToClipboard/index.js
@@ -11,10 +11,11 @@ import CopyToClipboard from 'react-copy-to-clipboard';
 import { Tooltip } from 'reactstrap';
 import StyledIconCopy from 'components/StyledIconCopy';
 import useSetTimeout from 'utils/useSetTimeout';
+import _uniqueId from 'lodash/uniqueId';
 
 function CopyToClipboardComponent(props) {
   const [tooltipOpen, setTooltipOpen] = useState(false);
-  const elementId = `c2c-${Date.now()}`;
+  const [id] = useState(_uniqueId('c2c-'));
 
   const { doTimer } = useSetTimeout(() => {
     setTooltipOpen(false);
@@ -26,18 +27,17 @@ function CopyToClipboardComponent(props) {
   };
 
   return (
-    <div>
+    <div className="d-inline">
       <CopyToClipboard text={props.value} onCopy={toggleTxTooltip}>
         <StyledIconCopy
-          className="d-inline-flex d-md-none"
           size={24}
-          id={elementId}
+          id={id}
         />
       </CopyToClipboard>
       <Tooltip
         hideArrow={props.hideArrow}
         isOpen={tooltipOpen}
-        target={elementId}
+        target={id}
       >
         {props.tooltip}
       </Tooltip>
