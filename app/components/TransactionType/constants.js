84bb89b908cba9c59da7575f010717868cc2549c
germanf <ferreyra.ger@gmail.com>
2021-06-08 12:29:28 -0300

    + added `TransactionType` component

---
+++ app/components/TransactionType/constants.js
@@ -0,0 +1,16 @@
+import { FEATURE_ACTIVATION_TYPE_INT } from 'containers/App/constants';
+
+//  transaction types without amount
+export const TX_TYPEINT_txWithoutAmount = [28, 53, 70, FEATURE_ACTIVATION_TYPE_INT];
+
+//  DEx Purchase
+export const TX_TYPEINT_DExPurchase = [-22];
+
+//  Send All
+export const TX_TYPEINT_SendAll = [4];
+
+//  OmniDex Trade transaction
+export const TX_TYPEINT_OmniDExTx = [25, 26];
+
+// Crowdsale Purchase
+export const TX_TYPE_CrowdsalePurchase = ['Crowdsale Purchase'];
---
+++ app/components/TransactionType/messages.js
@@ -0,0 +1,16 @@
+/*
+ * TransactionType Messages
+ *
+ * This contains all the text for the TransactionType component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.components.TransactionType';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'This is the TransactionType component!',
+  },
+});
