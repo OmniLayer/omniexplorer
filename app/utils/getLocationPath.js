957af2d08d5b6e99f4ea0585b722a4773b2dd8c8
germanf <ferreyra.ger@gmail.com>
2021-08-03 10:55:24 -0300

    + #refactoring added the new `OmniLite` ecosytem to getLocationPath helper

--- app/utils/getLocationPath.js
+++ app/utils/getLocationPath.js
@@ -1,12 +1,15 @@
 import {
+  API_OMNILITE_URL_BASE,
   API_OMNIFEATHER_URL_BASE,
   API_TESTNET_URL_BASE,
   API_URL_BASE,
 } from 'containers/App/constants';
 import isTestnet from './isTestnet';
 import isOmniFeather from './isOmniFeather';
+import isOmiLite from './isOmniLite';
 
 export default () => {
+
   if (isTestnet) {
     return API_TESTNET_URL_BASE;
   }
@@ -15,16 +18,24 @@ export default () => {
     return API_OMNIFEATHER_URL_BASE;
   }
 
+  if (isOmiLite) {
+    return API_OMNILITE_URL_BASE;
+  }
+
   return API_URL_BASE;
 };
 
 export const getSufixURL = () => {
-  if (window.location.href.includes('testnet')) {
+  if (isTestnet) {
     return '/testnet';
   }
 
-  if (window.location.href.includes('ftc')) {
+  if (isOmniFeather) {
     return '/ftc';
+  }
+
+  if (isOmiLite) {
+    return '/omnilite';
   }
 
   return '';
